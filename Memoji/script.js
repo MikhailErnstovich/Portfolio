0df3c5171af28290db4338d9e0cfd1364197f563
Mikhail <45947336+MikhailErnstovich@users.noreply.github.com>
2020-11-23 01:39:10 +0300

    Add files via upload
    CSS styles updated.

--- Memoji/script.js
+++ Memoji/script.js
@@ -1,12 +1,17 @@
 /*no trash in global scope*/
 (function(){
     const gameContainer = document.querySelector('.game-container');
-    const cellNumber = 24;
+    const gameName = 'Memoji';
+    const cellsQuantity = 24;
     const emojiPack = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐮", "🐷", "🐽", "🐸", "🐵", "🙈", "🙉", "🙊", "🐒", "🐔", "🐧", "🐦", "🐤", "🐣", "🐥", "🦆", "🦅", "🦉", "🦇", "🐺", "🐗", "🐴", "🦄", "🐝", "🐛", "🦋", "🐌", "🐞", "🐜", "🦟", "🦗", "🕷", "🕸", "🦂", "🐢", "🐍", "🦎", "🦖", "🦕", "🐙", "🦑", "🦐", "🦞", "🦀", "🐡", "🐠", "🐟", "🐬", "🐳", "🐋", "🦈", "🐊", "🐅", "🐆", "🦓", "🦍", "🦧", "🐘", "🦛", "🦏", "🐪", "🐫", "🦒", "🦘", "🐃", "🐂", "🐄", "🐎", "🐖", "🐏", "🐑", "🦙", "🐐", "🦌", "🐕", "🐩", "🦮", "🐕‍","🦺", "🐈", "🐓", "🦃", "🦚", "🦜", "🦢", "🦩", "🕊", "🐇", "🦝", "🦨"];
-    
-    let symbols = emojiPack.slice(0,cellNumber/2);
+    /*add game title*/
+    gameContainer.previousElementSibling.innerText = gameName;
+
+    /*create emoji array according to field size*/
+    let symbols = emojiPack.slice(0,cellsQuantity/2);
     symbols = symbols.concat(symbols).sort(() => Math.random() - 0.5);
 
+    /*constructor of cards*/
     class GameCard {
         constructor(){
             this.card;
@@ -17,11 +22,11 @@
             this.invalid = false;
         }
     }
-
+    /*game data and methods*/
     let gameObj = {
         container: gameContainer,
         cardsArr: [],
-        shuffleCards: function(arr){
+        addCardsValues: function(arr){
             this.cardsArr.forEach(function(el, num){
                 el.value = arr[num];
             });
@@ -30,86 +35,87 @@
             let cardElem = document.createElement('DIV');
             cardElem.classList.add('card');
             let cover = document.createElement('DIV');
-            cover.classList.add('card-cover','gray');
+            cover.classList.add('card-cover','gradient');
             let face = document.createElement('DIV');
-            face.classList.add('card-face','blue','rotY180');
+            face.classList.add('card-face','faceColor','rotY180');
             cardElem.append(face);
             cardElem.append(cover);
             container.append(cardElem);
             obj.card = cardElem;
             this.cardsArr.push(obj);
         },
+        createField: function(size, arr){
+            for(let i = 0; i < size; i++){
+                let x = new GameCard();
+                x.num = i;
+                this.createCard(this.container,x);
+                this.container.children[i].dataset.num = i;
+            }
+            this.addCardsValues(arr);
+        },
+        createOverlay:function(){
+            
+        },
+        gameLogic: function(event){
+            /*check click on card*/
+            let el = event.target;
+            let parent = el.parentNode;
+            if(!parent.classList.contains('card')){
+                return;
+            }
+            /*prevent click on opened cards*/
+            let num = parent.dataset.num;
+            let cardObj = gameObj.cardsArr[num];
+            if(cardObj.fixed || cardObj.inProcess || cardObj.invalid){
+                return;
+            }
+            /*turn card*/
+            let cardFace = parent.firstChild;
+            parent.classList.add('rotY180');
+            cardFace.innerText = cardObj.value;
+            cardObj.inProcess = true;
+            /*check cards*/
+            let arr = [];
+            gameObj.cardsArr.forEach(function(x){
+                if(x.inProcess){
+                    arr.push(x);
+                }
+                if(x.invalid){
+                    x.invalid = false;
+                    x.card.classList.remove('rotY180');
+                    x.card.firstChild.classList.remove('red');
+                    x.card.firstChild.classList.add('faceColor');
+                    /*user cant see answers in html*/
+                    //x.card.firstChild.innerText = "";
+                }
+            });
+            if(arr.length == 2){
+                arr[0].card.firstChild.classList.remove('faceColor');
+                arr[1].card.firstChild.classList.remove('faceColor');
+                if(arr[0].value == arr[1].value){
+                    arr[0].card.firstChild.classList.add('green');
+                    arr[1].card.firstChild.classList.add('green');
+                    arr[0].fixed = true;
+                    arr[1].fixed = true;
+                    arr[0].inProcess = false;
+                    arr[1].inProcess = false;
+                } else {
+                    arr[0].card.firstChild.classList.add('red');
+                    arr[1].card.firstChild.classList.add('red');
+                    arr[0].inProcess = false;
+                    arr[1].inProcess = false;
+                    arr[0].invalid = true;
+                    arr[1].invalid = true;
+                }
+            }
+        }
     };
+    gameObj.createField(cellsQuantity, symbols);
 
-    createField();
-    function createField(){
-        for(let i = 0; i < cellNumber; i++){
-            let x = new GameCard();
-            x.num = i;
-            gameObj.createCard(gameContainer,x);
-            gameContainer.children[i].dataset.num = i;
-        }
-        gameObj.shuffleCards(symbols);
-    }
-
-    function gameLogic(event){
-        /*check click on card*/
-        let el = event.target;
-        let parent = el.parentNode;
-        if(!parent.classList.contains('card')){
-            return;
-        }
-        /*check card status*/
-        let num = parent.dataset.num;
-        let cardObj = gameObj.cardsArr[num];
-        if(cardObj.fixed || cardObj.inProcess || cardObj.invalid){
-            return;
-        }
-        /*turn card*/
-        let cardFace = parent.firstChild;
-        parent.classList.add('rotY180');
-        cardFace.innerHTML = cardObj.value;
-        cardObj.inProcess = true;
-        /*check cards*/
-        let arr = [];
-        gameObj.cardsArr.forEach(function(x){
-            if(x.inProcess){
-                arr.push(x);
-            }
-            if(x.invalid){
-                x.invalid = false;
-                x.card.classList.remove('rotY180');
-                x.card.firstChild.classList.remove('red');
-                x.card.firstChild.classList.add('blue');
-                x.card.firstChild.innerHTML = "";
-            }
-        });
-        if(arr.length == 2){
-            arr[0].card.firstChild.classList.remove('blue');
-            arr[1].card.firstChild.classList.remove('blue');
-            if(arr[0].value == arr[1].value){
-                arr[0].card.firstChild.classList.add('green');
-                arr[1].card.firstChild.classList.add('green');
-                arr[0].fixed = true;
-                arr[1].fixed = true;
-                arr[0].inProcess = false;
-                arr[1].inProcess = false;
-            } else {
-                arr[0].card.firstChild.classList.add('red');
-                arr[1].card.firstChild.classList.add('red');
-                arr[0].inProcess = false;
-                arr[1].inProcess = false;
-                arr[0].invalid = true;
-                arr[1].invalid = true;
-            }
-        }
-    }
     gameContainer.addEventListener('click', function(){
-        gameLogic(event);
-    }
-    ,false);
+        gameObj.gameLogic(event);
+    });
 })();
-
 
 
 
